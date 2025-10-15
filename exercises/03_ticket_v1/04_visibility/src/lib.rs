278bc315d023e610f195b4c400706a6449ddad99
nickbp760 <nikochristian760@gmail.com>
2025-10-15 17:44:23 +0700

    release: ticket v1 cicil

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,12 +1,14 @@
 mod ticket {
-    struct Ticket {
+    pub struct Ticket {
         title: String,
         description: String,
         status: String,
     }
+    // bilang ke orang2 kalau ada kotak bernama ticket tapi orang lain ga boleh tau isinya karena masih private semua
 
     impl Ticket {
-        fn new(title: String, description: String, status: String) -> Ticket {
+        // kalau mau create ticket harus pakai new yang udah dibuat pub
+        pub fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
                 panic!("Title cannot be empty");
             }
@@ -36,6 +38,8 @@ mod ticket {
 //  in this exercise.
 #[cfg(test)]
 mod tests {
+    use crate::ticket;
+
     // TODO: Add the necessary `pub` modifiers in the parent module to remove the compiler
     //  errors about the use statement below.
     use super::ticket::Ticket;
@@ -43,6 +47,7 @@ mod tests {
     // Be careful though! We don't want this function to compile after you have changed
     // visibility to make the use statement compile!
     // Once you have verified that it indeed doesn't compile, comment it out.
+    #[test]
     fn should_not_be_possible() {
         let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
 
@@ -55,9 +60,10 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the line out to move on to the next exercise!
-        assert_eq!(ticket.description, "A description");
+        // assert_eq!(ticket.description, "A description");
     }
 
+    #[test]
     fn encapsulation_cannot_be_violated() {
         // This should be impossible as well, with a similar error as the one encountered above.
         // (It will throw a compilation error only after you have commented the faulty line
@@ -68,10 +74,16 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the lines out to move on to the next exercise!
-        let ticket = Ticket {
-            title: "A title".into(),
-            description: "A description".into(),
-            status: "To-Do".into(),
-        };
+        // let ticket = Ticket {
+        //     title: "A title".into(),
+        //     description: "A description".into(),
+        //     status: "To-Do".into(),
+        // };
+
+        // let _ticketnew = Ticket::new(
+        //     "My title".into(),
+        //     "A proper description".into(),
+        //     "To-Do".into(),
+        // );
     }
 }
