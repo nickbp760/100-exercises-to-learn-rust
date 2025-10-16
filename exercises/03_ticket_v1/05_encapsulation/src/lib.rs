b5d25ded8de7913d8e6f630ec6e1ce23f2992a60
nickbp760 <nikochristian760@gmail.com>
2025-10-16 11:35:57 +0700

    solusi: yay excercise 3 selesai

--- exercises/03_ticket_v1/05_encapsulation/src/lib.rs
+++ exercises/03_ticket_v1/05_encapsulation/src/lib.rs
@@ -30,10 +30,17 @@ pub mod ticket {
             }
         }
 
-        // TODO: Add three public methods to the `Ticket` struct:
-        //  - `title` that returns the `title` field.
-        //  - `description` that returns the `description` field.
-        //  - `status` that returns the `status` field.
+        pub fn title(self) -> String {
+            self.title
+        }
+
+        pub fn description(self) -> String {
+            self.description
+        }
+
+        pub fn status(self) -> String {
+            self.status
+        }
     }
 }
 
@@ -44,18 +51,18 @@ mod tests {
     #[test]
     fn description() {
         let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
-        assert_eq!(ticket.description, "A description");
+        assert_eq!(ticket.description(), "A description");
     }
 
     #[test]
     fn title() {
         let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
-        assert_eq!(ticket.title, "A title");
+        assert_eq!(ticket.title(), "A title");
     }
 
     #[test]
     fn status() {
         let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
-        assert_eq!(ticket.status, "To-Do");
+        assert_eq!(ticket.status(), "To-Do");
     }
 }
--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,63 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order{
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    pub fn product_name(&self) -> &String{
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u32{
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u32{
+        &self.unit_price
+    }
+    pub fn total(&self) -> u32{
+        self.quantity * self.unit_price
+    }
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(quantity);
+        Self::validate_unit_price(unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+    pub fn validate_product_name(product_name: &str){
+        if product_name.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if product_name.len() > 300 {
+            panic!("Title cannot be longer than 300 bytes");
+        }
+    }
+    pub fn validate_quantity(quantity: u32){
+        if quantity < 1{
+            panic!("The quantity must be strictly greater than zero")
+        }
+    }
+    pub fn validate_unit_price(unit_price: u32){
+        if unit_price <= 0{
+            panic!("The unit_price must be strictly greater than zero")
+        }
+    }
+    pub fn set_product_name(&mut self, product_name: String){
+        Self::validate_product_name(&product_name);
+        self.product_name = product_name;
+    }
+    pub fn set_quantity(&mut self, quantity: u32){
+        Self::validate_quantity(quantity);
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, unit_price: u32){
+        Self::validate_unit_price(unit_price);
+        self.unit_price = unit_price;
+    }
+}
