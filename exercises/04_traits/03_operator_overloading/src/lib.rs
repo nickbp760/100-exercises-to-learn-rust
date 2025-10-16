1f1b33a9e256a2505f0d919e01c8ab94ea81db90
nickbp760 <nikochristian760@gmail.com>
2025-10-16 16:13:24 +0700

    04: cicil exercises 4

--- exercises/04_traits/03_operator_overloading/src/lib.rs
+++ exercises/04_traits/03_operator_overloading/src/lib.rs
@@ -1,4 +1,4 @@
-use std::cmp::PartialEq;
+use std::{cmp::PartialEq, ptr::eq};
 
 struct Ticket {
     title: String,
@@ -8,7 +8,16 @@ struct Ticket {
 
 // TODO: Implement the `PartialEq` trait for `Ticket`.
 
-impl PartialEq for Ticket {}
+impl PartialEq for Ticket {
+    fn eq(&self, other: &Self) -> bool{
+        self.title == other.title && self.description == other.description && self.status == other.status
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !Self::eq(&self, &other)
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
