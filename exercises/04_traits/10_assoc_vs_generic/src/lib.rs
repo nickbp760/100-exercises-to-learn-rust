8ecfe94c21ffa0793aeb5ef23d70c9dd8fd67913
nickbp760 <nikochristian760@gmail.com>
2025-10-17 10:03:35 +0700

    solusi: pusing juga 04 ini

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -1,17 +1,31 @@
-// TODO: Define a new trait, `Power`, that has a method `power` that raises `self`
-//  to the power of `n`.
-//  The trait definition and its implementations should be enough to get
-//  the tests to compile and pass.
-//
-// Recommendation: you may be tempted to write a generic implementation to handle
-// all cases at once. However, this is fairly complicated and requires the use of
-// additional crates (i.e. `num-traits`).
-// Even then, it might be preferable to use a simple macro instead to avoid
-// the complexity of a highly generic implementation. Check out the
-// "Little book of Rust macros" (https://veykril.github.io/tlborm/) if you're
-// interested in learning more about it.
-// You don't have to though: it's perfectly okay to write three separate
-// implementations manually. Venture further only if you're curious.
+
+// kalau pakai assoc type disini power for u32 cuma bisa 1
+// sementara disini kita butuh untuk 2 input yaitu u32 dan u16
+// makanya orang biasa pakai generic type
+pub trait Power<N> {
+    fn power(self, n: N) -> Self;
+}
+
+// implementasi untuk u32 ^ u32
+impl Power<u32> for u32 {
+    fn power(self, n: u32) -> Self {
+        self.pow(n)
+    }
+}
+
+// implementasi untuk u32 ^ u16
+impl Power<u16> for u32 {
+    fn power(self, n: u16) -> Self {
+        self.pow(n as u32)
+    }
+}
+
+// implementasi untuk u32 ^ &u32 (reference)
+impl Power<&u32> for u32 {
+    fn power(self, n: &u32) -> Self {
+        self.pow(*n)
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,8 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+// secara aturan Rust, kamu tidak bisa Copy tanpa Cloneala
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +10,16 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+// yang pakai penambahan adalah struct WrappingU32 jadi butuh impl add
+// karena tipe buat for u32 pasti input u32 jadi cukup pakai assoc type
+impl std::ops::Add for WrappingU32 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.value.wrapping_add(rhs.value))
     }
 }
 
@@ -20,6 +32,7 @@ mod tests {
         let x = WrappingU32::new(42);
         let y = WrappingU32::new(31);
         let z = WrappingU32::new(u32::MAX);
+        // + y kedua jadi butuh copy
         assert_eq!(x + y + y + z, WrappingU32::new(103));
     }
 }
