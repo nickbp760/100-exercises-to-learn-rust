1e0b6cac509dd27db69c44ce092d31a64e757a04
nickbp760 <nikochristian760@gmail.com>
2025-10-19 16:06:50 +0700

    06: cicil2 tipis

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,13 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+pub fn squared(slice: &mut [u32]) {
+    for x in slice.iter_mut() {
+        *x *= *x;
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
